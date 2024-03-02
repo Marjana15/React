388d567e1a0ad9f24155dfc6cde2a74a5e38f35e
Marjana <marjanamarjanabegum5@gmail.com>
2024-03-02 15:03:02 +0600

    Update Result.jsx

--- Day 5/src/components/Result.jsx
+++ Day 5/src/components/Result.jsx
@@ -1,8 +1,7 @@
-import { calculateInvestmentResults } from '../util/investment.js';
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 export default function Results({ input }) {
     const resultsData = calculateInvestmentResults(input);
-
     const initialInvestment =
         resultsData[0].valueEndOfYear -
         resultsData[0].interest -
