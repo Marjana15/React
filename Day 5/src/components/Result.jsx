c31ad0fc9fde58e4a8b4f328442433ff44c2d2c3
Marjana <marjanamarjanabegum5@gmail.com>
2024-03-02 15:02:26 +0600

    Update Result.jsx

--- Day 5/src/components/Result.jsx
+++ Day 5/src/components/Result.jsx
@@ -5,5 +5,18 @@ export default function Results({ input }) {
 
     console.log(resultsData);
 
-    return <p>Results...</p>
+    return (
+        <table id="result">
+        <thead>
+            <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+            </tr>
+        </thead>
+        
+        </table>
+    );
 }
